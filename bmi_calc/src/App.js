9bfb3f985b5b06254e20c4c44fd03724a186269c
@ <2060newty@gmail.com>
2023-03-18 21:58:41 +0300

    just the JSX
    made the structure of the app. I want to begin on styling it before the functionality

--- bmi_calc/src/App.js
+++ bmi_calc/src/App.js
@@ -1,10 +1,32 @@
 
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>BMI CALCULATOR</h1>
+      <div className="main-container">
+        <h1>BMI CALCULATOR</h1>
+        <h3>Height(m)</h3>
+        <form>
+          <input type="text"></input>
+        </form>
+        <h3>Weight(kg)</h3>
+        <form>
+          <input type="text"></input>
+        </form><br />
+        <div className="btns">
+          <button type="submit"> Sumbit</button>
+          <br /><br />
+          <button type="submit"> Reload</button>
+
+        </div>
+
+        <div className="pic-container">
+          <h2>Your BMI is:</h2>
+        </div>
+
+
+      </div>
+
     </div>
   );
 }
