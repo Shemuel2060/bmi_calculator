a2bb99484cad874b454d7d096ded2bd93a6d88a1
@ <2060newty@gmail.com>
2023-03-19 16:19:16 +0300

    some CSS styling

--- bmi_calc/src/App.js
+++ bmi_calc/src/App.js
@@ -1,32 +1,45 @@
-
+import "./App.css";
+import { useState } from 'react';
 
 function App() {
+
+  // state
+  const [weight, setWeight] = useState(0);
+  const [height, setHeight] = useState(0);
+  const [bmi, setBmi] = useState(" ");
+  const [message, setMessage] = useState(" ")
+
+  let imgSrc = '';
+
+
+
+
   return (
     <div className="App">
       <div className="main-container">
-        <h1>BMI CALCULATOR</h1>
-        <h3>Height(m)</h3>
+        <h1 className="App-header">BMI CALCULATOR</h1>
         <form>
-          <input type="text"></input>
+          <label>Height (m)</label><br /><br />
+          <input value={height}></input><br /><br />
+          <label>Weight (kg)</label><br /><br />
+          <input value={weight} />
+          <br />
+          <div >
+            <br />
+            <button className="btn-submit" type="submit"> Sumbit</button>
+            <br /><br />
+            <button className="btn-reload" type="submit"> Reload</button>
+          </div>
         </form>
-        <h3>Weight(kg)</h3>
-        <form>
-          <input type="text"></input>
-        </form><br />
-        <div className="btns">
-          <button type="submit"> Sumbit</button>
-          <br /><br />
-          <button type="submit"> Reload</button>
 
+        <div>
+          <h3>Your BMI is: {bmi}</h3>
+          <p>{message}</p>
         </div>
-
-        <div className="pic-container">
-          <h2>Your BMI is:</h2>
+        <div className="img-show">
+          <img src={imgSrc} alt="" />
         </div>
-
-
       </div>
-
     </div>
   );
 }
