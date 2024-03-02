1f24bbfc525a1778d989682249685508fca06fde
Shemuel2060 <2060newty@gmail.com>
2024-03-02 21:44:16 +0300

    removed backend folder which was empty

--- bmi_backend/bmi_calc/src/main/java/shemuel/bmi/bmi_calc/repository/PatientRepo.java
+++
@@ -1,9 +0,0 @@
-package shemuel.bmi.bmi_calc.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import shemuel.bmi.bmi_calc.model.patient;
-
-public interface PatientRepo extends JpaRepository<patient,Integer>{
-    
-}
